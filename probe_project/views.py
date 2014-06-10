6c7a809e3700c688069f5739a9f8da9f80ebf2c2
Gabriel Le Breton <lebreton.gabriel@gmail.com>
2014-06-10 11:43:48 -0400

    I am so hot right now (login and register now working! took a while but its now f****** awesome) :)

--- probe_project/views.py
+++ probe_project/views.py
@@ -7,6 +7,7 @@ from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth import authenticate, logout
 from django.contrib.auth.forms import UserCreationForm
+from userena.forms import AuthenticationForm
 
 from probe_project.apps.probe_dispatcher import views
 
@@ -16,7 +17,7 @@ def home(request):
     if request.user.is_active:
         return redirect(views.dashboard)
     else:
-        return render_to_response("home.html", RequestContext(request, {'app': 'Probe'}))
+        return render_to_response("home.html", RequestContext(request, {'app': 'Probe', 'form': AuthenticationForm}))
 
 
 @csrf_protect
@@ -25,9 +26,11 @@ def custom_login(request):
         user = authenticate(username=request.POST.get('UsernameEmail'), password=request.POST.get('password'))
         if user is not None:
             if user.is_active:
-                return redirect(reverse(views.dashboard), context_instance=RequestContext(request, { 'message': 'Welcome back!'}))
+                return redirect(reverse(views.dashboard),
+                                context_instance=RequestContext(request, {'message': 'Welcome back!'}))
             else:
-                return render_to_response("login.html", RequestContext(request, {'error': _('User has been disabled, please contact us for more details')}))
+                return render_to_response("login.html", RequestContext(request, {
+                'error': _('User has been disabled, please contact us for more details')}))
         else:
             return render_to_response("login.html", RequestContext(request, {'error': _('Invalid user or password')}))
 
@@ -44,11 +47,12 @@ def custom_register(request):
                 new_user = form.save()
                 return redirect(reverse(views.dashboard), {'message': _('Welcome to your new account!')})
             else:
-                return render_to_response("register.html", RequestContext(request, {'form':form, 'terms_error': _("You must accept the terms and conditions")}))
+                return render_to_response("register.html", RequestContext(request, {'form': form, 'terms_error': _(
+                    "You must accept the terms and conditions")}))
 
     else:
         form = UserCreationForm()
-    return render_to_response("register.html", RequestContext(request, {'form':form}))
+    return render_to_response("register.html", RequestContext(request, {'form': form}))
 
 
 def custom_logout(request, next_page):
