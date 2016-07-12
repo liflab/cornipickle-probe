f311bb1ce41a3cca4c1bc5d31376dc02df611c27
Faterou <francis_tofu@hotmail.com>
2016-07-12 14:34:38 -0400

    Updated jquery and bootstrap. added jquery to base template.

--- probe_project/local_static/js/fiddle/fiddle.js
+++ probe_project/local_static/js/fiddle/fiddle.js
@@ -1,8 +1,15 @@
-var open = false;
-$('#editorYellowButton').click(function() {
-  $('#editorDropup').animate({
-    top:'-100px'
-  }, 500, function() {
-    open = true;
-  });
-});
+(function($){
+    var open = false;
+    $('#editorYellowButton').click(function() {
+        $('#editorDropup').animate({
+            top:'-100px'
+        }, 500, function() {
+            open = true;
+        });
+    });
+
+    window.onload = function() {
+        var t = $(".fiddleEditor");
+    };
+})(jQuery);
+
