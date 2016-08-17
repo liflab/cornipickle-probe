639ab5ba5c8e0c4719f497a11de3eb340f6abdbe
daehli1 <daehlinadeauotis@gmail.com>
2016-08-17 16:02:40 -0400

    Ajout de l'objet Button + modification du Css pour permettre l'apparition du button

--- probe_project/local_static/js/fiddle/fiddle.js
+++ probe_project/local_static/js/fiddle/fiddle.js
@@ -1,18 +1,66 @@
 var open = false;
 
-window.onload = function() {
-    $(".fiddleEditor").each( function () {
-        var text = $(this)[0].firstElementChild.value;
-        var id = $(this)[0].firstElementChild.id;
-        var name = $(this)[0].firstElementChild.getAttribute("name");
-        $(".fiddleEditor").load("http://localhost:8000/fiddle/fiddleeditor",{"text":text, "id":id, "name":name});
+/*
 
-    })
+ */
+var Button = function (elementNaming) {
+
+    this.button = document.createElement("button");
+
+    this.naming =  document.getElementsByClassName(elementNaming);
+
+    this.style = []; // Style a ajouter
+
+    this.color = ""; // Color
+
+    this.statut = ""; // Open & Close
+
+    this.type = "";
+
+
+
+    var Button  = function(elementNaming){
+        addButton();
+    };
+
+    var privateMethod = function () {
+        // private
+    };
+
+    var applyStyle = function () {
+        button.style = this.style
+    };
+
+
+
+    var addButton = function() {
+        this.naming.appendChild(this.button);
+    };
+
+    var addStyle = function (stylebuttom) {
+        // public
+        style.push(stylebuttom)
+
+
+
+    };
+
+    var anotherMethod = function () {
+        // public
+    };
+
+    return {
+        Button:Button,
+        anotherMethod: anotherMethod
+    };
+
+
 };
+
 
 $('body').on('click', ".editorYellowButton", function() {
     if(!open) {
-        $(".glyphicon-chevron-down").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up")
+        $(".glyphicon-chevron-up").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down")
         $('.editorDropup').animate({
             top: '-=' + $('.editorPropertyMetadata').height() + 'px'
         }, 300, function() {
@@ -20,7 +68,7 @@ $('body').on('click', ".editorYellowButton", function() {
         });
     }
     else {
-        $(".glyphicon-chevron-up").removeClass("glyphicon-chevron-up").addClass("glyphicon-chevron-down")
+        $(".glyphicon-chevron-down").removeClass("glyphicon-chevron-down").addClass("glyphicon-chevron-up")
         $('.editorDropup').animate({
             top: '+=' + $('.editorPropertyMetadata').height() + 'px'
         }, 300, function() {
@@ -28,4 +76,19 @@ $('body').on('click', ".editorYellowButton", function() {
         });
     }
 });
+
+
+window.onload = function() {
+    $(".fiddleEditor").each( function () {
+        var text = $(this)[0].firstElementChild.value;
+        var id = $(this)[0].firstElementChild.id;
+        var name = $(this)[0].firstElementChild.getAttribute("name");
+        $(".fiddleEditor").load("http://localhost:8000/fiddle/fiddleeditor",{"text":text, "id":id, "name":name});
+
+        var button = Button.Button("editorContainer");
+    });
+
+
+    
+};
 
