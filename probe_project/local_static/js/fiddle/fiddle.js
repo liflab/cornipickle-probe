2c0b0a349616b554b33cef9a9dfbee7567ef0492
daehli1 <daehlinadeauotis@gmail.com>
2016-08-24 16:53:19 -0400

    Ajout du bouton Group (Raw, Cornipickle)

--- probe_project/local_static/js/fiddle/fiddle.js
+++ probe_project/local_static/js/fiddle/fiddle.js
@@ -1,42 +1,5 @@
 var open = false;
 
-/*
- Class Button qui Creer un Element Button Dynamiquement
-
- elementNaming: Class sur lequel le button dois être ajouté. Faite bien attention de l'ajouter avec un Classe et non un ID
-
- */
-/*
-
- var Button = function (elementNaming) {
-
- this.button = document.createElement("button");
-
- this.naming =  this.detectNaming(elementNaming) || "";
-
- this.detectNaming = function(name) {
- if(typeof document.getElementsByClassName(name)[0] !== "undefined" ){
- this.naming = document.getElementsByClassName(name)[0];
- }
- else if (typeof document.getElementById(name) !== "undefined") {
- this.naming = document.getElementById(name);
- }
- else {
- console.log("Veuiller regarder la sytaxe de l'element dans le premier Parametre");
- }
- };
- this.addButton = function() {
- this.button.innerHTML = "Click ME";
- this.naming.appendChild(this.button);
- };
-
- this.getInfo = function() {
- console.log("Naming: "+this.naming +"\n button : "+ this.button + "\n");
- };
-
- };
- */
-
 var ListOfEditors = [];
 
 var CodeMirrorEditor = function() {
@@ -44,20 +7,25 @@ var CodeMirrorEditor = function() {
 
     this.insertion = function(element) {
         this.m_editor = CodeMirror.fromTextArea(element,{
-        lineNumbers:true,
-        mode: "xml",
-        lineWrapping:true,
-        theme: "hopscotch"
-        // Nous devons Creer le highlight de Cornipickle et le mettre ici
-        // Nous devons télécharger notre propre codemirroir
+            lineNumbers:true,
+            mode: "cornipickle",
+            lineWrapping:true,
+            theme: "rubyblue"
+            // Nous devons Creer le highlight de Cornipickle et le mettre ici
+            // Nous devons télécharger notre propre codemirroir
         });
 
         this.m_editor.on("change", this.onChange);
-    }
+    };
 
     this.onChange = function(codemirror, obj) {
         console.log("EVENT");
     };
+
+    this.restore = function() {
+        var restore = this.m_editor.getTextArea();
+        this.insertion(restore);
+    }
 };
 
 $('body').on('click', ".editorYellowButton", function() {
@@ -84,6 +52,7 @@ $('body').on('click', ".editorYellowButton", function() {
 
 window.onload = function() {
     $(".fiddleEditor").each( function () {
+        var count = 0;
         var t = $(this)[0];
         var text = t.firstElementChild.value;
         var id = t.firstElementChild.id;
@@ -95,19 +64,29 @@ window.onload = function() {
             data: {"text":text, "id":id, "name":name},
             success: function(html) {
                 var h = document.createElement("div");
+                h.id = "codeMirrorInstance" + count; // Je donne ici un Id a un nouvelle élément
                 h.innerHTML = html;
                 t.firstElementChild.parentNode.replaceChild(h,t.firstElementChild);
                 var selector = "#" + id;
                 var newEditor = new CodeMirrorEditor();
                 newEditor.insertion($(selector)[0]);
 
-                var button = new Button($(selector)[0].id);
-                button.addId("changeMode");
-                button.addFunctionOnClick();
-                button.addButton();
+                $(".rawbutton").click(function () {
+                    newEditor.m_editor.setOption("theme","hopscotch");
+                    $(this).prop('disabled',true);
+                    $(".cornipickleButton").prop('disabled',false)
+                });
+
+                $(".cornipickleButton").click(function () {
+                    newEditor.m_editor.setOption("theme","rubyblue");
+                    $(".rawbutton").prop('disabled',false);
+                    $(this).prop('disabled',true);
+
+                });
                 ListOfEditors.push(newEditor);
             }
         });
+        count++;
     });
 };
 
